16ecd5af04bfd29f0ce0cd7f26a15f52ccf45f3e
victorclee <victorchristopherlee@gmail.com>
2024-01-25 21:02:37 -0600

    whole song complete

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,38 @@
 class Bottles
+
+  def song
+    verses(99,0)
+  end
+
+  def verses(max, min)
+    max.downto(min).collect {|i| verse(i)}.join("\n")
+  end
+
   def verse(number)
-    "99 bottles of beer on the wall, " +
-    "99 bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "98 bottles of beer on the wall.\n"
+    case number
+    when 2
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number - 1} bottle of beer on the wall.\n"
+    when 1
+      "#{number} bottle of beer on the wall, " +
+      "#{number} bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +      
+      "Take one down and pass it around, " +
+      "#{number - 1} bottles of beer on the wall.\n"
+    end
+
+
   end
 
 end
